42a64ef10c190c3280a51cb4ef3c8d1b6c693788
Lenix <149199196+lenix-x@users.noreply.github.com>
2025-05-27 02:56:36 +0100

    add fd_banking system

--- modules/managment/fd_banking/server.lua
+++ modules/managment/fd_banking/server.lua
@@ -1,32 +1,25 @@
----@diagnostic disable: duplicate-set-field
-if GetResourceState('fd_banking') == 'missing' then return end
+if GetResourceState('fd_banking') ~= 'started' then return end
 Managment = Managment or {}
 
-local fd_banking = exports['fd_banking']
-
----This will return a table with account details
----@param account string
----@return table
-Managment.GetAccountMoney = function(account)
-    return fd_banking:GetAccount(account)
+---@param identifier<string> - Identifier (citizen id)
+Managment.GetAccountMoney = function(citizenId)
+    return exports.fd_banking:getPersonalAccount(identifier).balance
 end
 
 ---This will add money to the specified account of the passed amount
----@param account string
----@param amount number
----@param reason string
----@return boolean
-Managment.AddAccountMoney = function(account, amount, reason)
-    return fd_banking:AddMoney(account, amount, reason)
+---@param society<string> - society identifier
+---@param amount<number> - amount
+---@param reason<string> - reason
+Managment.AddAccountMoney = function(account, amount)
+    return exports.fd_banking:AddMoney(society, amount, reason)
 end
 
 ---This will remove money from the specified account of the passed amount
----@param account string
----@param amount number
----@param reason string
----@return boolean
-Managment.RemoveAccountMoney = function(account, amount, reason)
-    return fd_banking:removeAccountMoney(account, amount, reason)
+---@param society<string> - society identifier
+---@param amount<number> - amount
+---@param reason<string> - reason
+Managment.RemoveAccountMoney = function(account, amount)
+    return exports.fd_banking:RemoveMoney(society, amount, reason)
 end
 
 return Managment
