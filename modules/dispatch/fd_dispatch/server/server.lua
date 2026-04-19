b8642cea23e8cc10c30d16a7bb00cebdaa43da69
Luke <62490330+Luke20201@users.noreply.github.com>
2026-04-19 17:10:57 +0100

    Fix blips

--- modules/dispatch/fd_dispatch/server/server.lua
+++ modules/dispatch/fd_dispatch/server/server.lua
@@ -21,7 +21,7 @@ Dispatch.SendAlert = function(data)
     if type(data.job) == "string" then
         data.job = {data.job}
     end
-    local coords = data.coords or nil
+    local coords = data.coords or GetEntityCoords(GetPlayerPed(data.source))
     local alertData = {
         source = data.source,
         title = data.title or "No message provided",
@@ -31,11 +31,15 @@ Dispatch.SendAlert = function(data)
         priority = data.priority or 2,
         metadata = metadata,
         blip = {
-            radius = data.blip and data.blip.radius or 0,
+            title = data.title or "No message provided",
+            coords = coords,
+            radius = data.blip and data.blip.radius or nil,
             sprite = data.blip and data.blip.sprite or 161,
-            color = data.blip and data.blip.color or 84,
-            scale = data.blip and data.blip.scale or 1.0,
-            time  = data.time and (data.time / 1000) or nil,
+            color = data.blip and data.blip.color or 1,
+            scale = data.blip and data.blip.scale or 1.2,
+            flashes = data.blip.flash or false,
+            time = data.time or (6 * 60000),
+            category = data.category or nil,
         },
     }
     exports.fd_dispatch:addAlert(alertData)
